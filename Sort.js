288a8a61e85bdde5cac12d4ead9d8343132e4b5b
Buyers <68842704+gothubug@users.noreply.github.com>
2020-07-30 18:37:26 +0800

    Update Sort.js

--- Sort.js
+++ Sort.js
@@ -1,1 +1,12 @@
-
+function sort(nums)  {
+    for (i = 0; i < nums.length; i++) {
+        for (j = i; j < nums.length; j++) {
+            if (nums[j] <= nums[i]) {
+                var tmp = nums[i];
+                nums[i] = nums[j];
+                nums[j] = tmp;
+            }
+        }
+    }
+    return nums;
+}
