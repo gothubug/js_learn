e723a2863376daf2403e47f041c99f6bbcb05b8c
Buyers <68842704+gothubug@users.noreply.github.com>
2020-11-12 16:08:58 +0800

    Create chengfakoujue.js

---
+++ chengfakoujue.js
@@ -0,0 +1,9 @@
+function cfkj() {
+    for(var i=1; i < 10; i++) {
+        var result = "";
+        for(var j=1; j<i+1; j++) {
+            result += j+"×"+i+"="+i*j+" ";
+        }
+        console.log(result);
+    }
+}
